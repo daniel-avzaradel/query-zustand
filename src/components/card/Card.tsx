4881de0a47688c1114bf6fe9a13ac7bd67a79bc4
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-07-16 22:52:23 +0300

    added more styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,18 +1,18 @@
-import { Repo } from '../../queries/repo/types'
+import {Repo} from '../../queries/repo/types'
 import './styles.css'
 
 interface CardProps {
     repo: Repo
 }
 
-const Card = ({repo}: CardProps) => {
+const Card = ({repo} : CardProps) => {
 
-  return (
+    return (
     <div className='card'>
-        {repo.name}
+        <div>{repo.name}</div>
         <button className="btn">Favorite</button>
     </div>
-  )
+    )
 }
 
 export default Card
